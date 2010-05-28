93fd3c40bec821b792131058634160abbcdbb0e0
Randy Zauhar <zauhar@Artemis-Discovery-Randy-Zauhar.local>
2010-05-28 10:25:25 -0400

    forgot to add platform.h, which controls OSX vs Linux compile

---
+++ platform.h
@@ -0,0 +1,14 @@
+/*
+ *  platform.h
+ *  ShapeSignaturesX2
+ *
+ *  Created by Randy Zauhar on 5/18/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define OSX
+//#define LINUX
+//#define TRUE 1
+//#define FALSE 0
+
